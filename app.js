cbf3d5b68c213d8f9175cbfaafa0a54811ff1ec2
AKASH VERMA <aakashverma4102@gmail.com>
2023-01-16 17:11:27 +0530

    adding google authentication

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ const mongoose =require("mongoose");
 const session=require('express-session');
 const passport=require('passport');
 const passportLocalMongoose=require('passport-local-mongoose');
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app=express();
 
@@ -43,22 +45,61 @@ mongoose.connect("mongodb://127.0.0.1/userDB",{useNewUrlParser:true},function(er
 
 const userSchema=new mongoose.Schema({
     email: String,
-    password: String
+    password: String,
+    googleId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
-
+userSchema.plugin(findOrCreate);
 // userSchema.plugin(encrypt,{secret:process.env.SECRET, encryptedFields:['password']});
 
 const User=new mongoose.model("User",userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+// passport.serializeUser(User.serializeUser());
+// passport.deserializeUser(User.deserializeUser());
+// used to serialize the user for the session
+passport.serializeUser(function(user, done) {
+    done(null, user.id); 
+   // where is this user.id going? Are we supposed to access this anywhere?
+});
+
+// used to deserialize the user
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",(req,res)=>{
     res.render("home");
 });
+
+app.get("/auth/google",
+    passport.authenticate("google" , {scope: ["profile"]})
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/secrets');
+});
+
 app.get("/login",(req,res)=>{
     res.render("login");
 });
@@ -147,6 +188,7 @@ app.post("/login",(req,res)=>{
     });
 
 });
+
 
 
 
